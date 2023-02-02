9fc2d38eb0d271ff65d3d1176f5ee50378db1749
Adam Angel <adamangel@Adams-MacBook-Pro.local>
2023-02-02 07:21:30 -0500

    devise views added, first name and last name added to users as strings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
   devise_for :users
   resources :courses
-  root 'home#index'
+
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
