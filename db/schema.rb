e616d9c61173b9073fb9ce0de8df893bf29eb8b7
Adam Angel <adamangel@Adams-MacBook-Pro.local>
2023-02-02 07:31:11 -0500

    added courses to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_02_120856) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_122855) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -54,6 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_120856) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -72,4 +74,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_120856) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "courses", "users"
 end
